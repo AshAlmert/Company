14cd2abe55962e743409f48ec2e9c87ee1c9ec12
AshAlmert <90694527+AshAlmert@users.noreply.github.com>
2021-12-26 22:55:05 +0300

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,6 @@
+/*src=newFunction()
+
+function newFunction() {
+    return "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9a196d0b3feb096f6b800f4d0b88cf6afe3c3ffe8635b1c7b06b6e5f054398ce&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true";
+}
+*/
